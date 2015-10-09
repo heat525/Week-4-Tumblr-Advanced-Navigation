46734279fc2a561c56f8ccb5cd11b6192c878239
heat525 <prime.heather@gmail.com>
2015-10-09 15:11:41 -0700

    Initial Commit

---
+++ Week 4 Tumblr Advanced NavigationTests/Week_4_Tumblr_Advanced_NavigationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_4_Tumblr_Advanced_NavigationTests.swift
+//  Week 4 Tumblr Advanced NavigationTests
+//
+//  Created by Prime, Heather(AWF) on 10/9/15.
+//  Copyright © 2015 Prime, Heather. All rights reserved.
+//
+
+import XCTest
+@testable import Week_4_Tumblr_Advanced_Navigation
+
+class Week_4_Tumblr_Advanced_NavigationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
